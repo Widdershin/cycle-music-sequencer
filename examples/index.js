7ba31a33637f8450d8fa215c2eba2bfb49f90cc1
Nick Johnstone <ncwjohnstone@gmail.com>
2015-12-17 11:05:27 +1300

    Some simple notes + live reloading server

--- examples/index.js
+++ examples/index.js
@@ -3,3 +3,7 @@ import Tone from 'tone';
 const synth = new Tone.SimpleSynth().toMaster();
 
 synth.triggerAttackRelease("C4", "8n");
+
+synth.triggerAttackRelease("E4", "8n", "0:1");
+
+synth.triggerAttackRelease("F4", "8n", "0:2");
