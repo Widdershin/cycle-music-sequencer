553cbe9f77b0838ff255aa943cfe190a9849c817
Rachel Collingridge <rachel@powershop.co.nz>
2015-12-17 15:36:55 +1300

    Make a music driver

--- examples/index.js
+++ examples/index.js
@@ -9,11 +9,6 @@ const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
 const major_scale_pattern = [2, 2, 1, 2, 2, 2, 1];
 const harmonic_minor_scale_pattern = [2, 1, 2, 2, 1, 3, 1];
 
-// Observable
-//   .interval(bpm(120))
-//   .take(notes.length)
-//   .subscribe(i => synth.triggerAttackRelease(note(i + notes.length * 2), "4n"));
-
 function bpm (tempo) {
   return (1000 * 60) / tempo;
 }
@@ -22,29 +17,45 @@ function note (i) {
   const octave = Math.floor(i / notes.length) + 1;
   const noteIndex = i % notes.length;
 
-  console.log(notes[noteIndex] + octave);
   return notes[noteIndex] + octave;
 }
 
 function main ({DOM}) {
-  const clickAdd = DOM.select(".add").events("click").map(event => 1);
-  const clickSubtract = DOM.select(".subtract").events("click").map(event => -1);
-  const change = Observable.merge(clickAdd, clickSubtract);
-  const count = change.scan((total, change) => total + change).startWith(0);
+  const startingNoteIndex = notes.length * 2
 
-  count.forEach(i => synth.triggerAttackRelease(note(i + notes.length * 2), "4n"));
+  const clickAdd$ = DOM
+    .select(".add")
+    .events("click")
+    .map(event => 1);
+
+  const clickSubtract$ = DOM
+    .select(".subtract")
+    .events("click")
+    .map(event => -1);
+
+  const change$ = Observable.merge(
+    clickAdd$,
+    clickSubtract$
+  );
+
+  const count$ = change$
+    .scan((total, change) => total + change, startingNoteIndex)
+    .startWith(startingNoteIndex);
 
   return {
-    DOM: count.map(count =>
+    DOM: count$.map(count =>
       h(".counter", [
         h("button.subtract", "-"),
-        "Count: " + count,
+        "Note: " + note(count),
         h("button.add", "+")
       ])
-    )
+    ),
+
+    music$: count$.map(note)
   };
 }
 
 run(main, {
-  DOM: makeDOMDriver(".app")
+  DOM: makeDOMDriver(".app"),
+  music$: note$ => note$.forEach(note => synth.triggerAttackRelease(note, "4n"))
 });
