7a10309b745c4d35096a573768830403f040481a
Rachel Collingridge <rachel@powershop.co.nz>
2015-12-17 14:42:34 +1300

    Cycle 'Hello World'

--- examples/index.js
+++ examples/index.js
@@ -1,21 +1,24 @@
 import Tone from 'tone';
-import {Observable} from 'rxjs';
+import {Observable} from 'rx';
+import {run} from '@cycle/core';
+import {makeDOMDriver, h} from '@cycle/dom';
 
 // const synth = new Tone.PolySynth(4, Tone.MonoSynth).toMaster();
 const synth = new Tone.SimpleSynth().toMaster();
 const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+const major_scale_pattern = [2, 2, 1, 2, 2, 2, 1];
+const harmonic_minor_scale_pattern = [2, 1, 2, 2, 1, 3, 1];
 
-Observable
-  .interval(bpm(120))
-  .take(notes.length)
-  .subscribe(i => synth.triggerAttackRelease(note(i + notes.length * 2), "4n"));
+// Observable
+//   .interval(bpm(120))
+//   .take(notes.length)
+//   .subscribe(i => synth.triggerAttackRelease(note(i + notes.length * 2), "4n"));
 
 function bpm (tempo) {
   return (1000 * 60) / tempo;
 }
 
 function note (i) {
-  // debugger
   const octave = Math.floor(i / notes.length) + 1;
   const noteIndex = i % notes.length;
 
@@ -23,8 +26,12 @@ function note (i) {
   return notes[noteIndex] + octave;
 }
 
-// synth.triggerAttackRelease("C4", "4n");
+function main () {
+  return {
+    DOM: Observable.just(h(".hello-world", "Hello World"))
+  };
+}
 
-// synth.triggerAttackRelease(["C4", "E4"], "4n");
-
-// synth.triggerAttackRelease("F4", "4n", "0:2");
+run(main, {
+  DOM: makeDOMDriver(".app")
+});
