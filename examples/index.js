756c544ba81822692e42dccd6a9684ae535a04f3
Rachel Collingridge <rachel@powershop.co.nz>
2015-12-17 16:51:02 +1300

    Create grid for sequence

--- examples/index.js
+++ examples/index.js
@@ -6,8 +6,6 @@ import {makeDOMDriver, h} from '@cycle/dom';
 // const synth = new Tone.PolySynth(4, Tone.MonoSynth).toMaster();
 const synth = new Tone.SimpleSynth().toMaster();
 const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-const major_scale_pattern = [2, 2, 1, 2, 2, 2, 1];
-const harmonic_minor_scale_pattern = [2, 1, 2, 2, 1, 3, 1];
 
 function bpm (tempo) {
   return (1000 * 60) / tempo;
@@ -21,38 +19,60 @@ function note (i) {
 }
 
 function main ({DOM}) {
-  const startingNoteIndex = notes.length * 2
+  const initialState = {
+    score: [
+      {note: 'A4', beats: [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]},
+      {note: 'G4', beats: [0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]},
+      {note: 'F4', beats: [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0]},
+      {note: 'E4', beats: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]},
+      {note: 'D4', beats: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0]},
+      {note: 'C4', beats: [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]}
+    ],
+    playing: true,
+    beat: 2
+  }
 
-  const clickAdd$ = DOM
-    .select(".add")
-    .events("click")
-    .map(event => 1);
-
-  const clickSubtract$ = DOM
-    .select(".subtract")
-    .events("click")
-    .map(event => -1);
-
-  const change$ = Observable.merge(
-    clickAdd$,
-    clickSubtract$
-  );
-
-  const count$ = change$
-    .scan((total, change) => total + change, startingNoteIndex)
-    .startWith(startingNoteIndex);
+  const state$ = Observable.just(initialState);
 
   return {
-    DOM: count$.map(count =>
-      h(".counter", [
-        h("button.subtract", "-"),
-        "Note: " + note(count),
-        h("button.add", "+")
+    DOM: state$.map(state =>
+      h(".score", [
+        renderBeatMarkerRow(state.beat),
+        renderScoreGrid(state.score)
       ])
     ),
 
-    music$: count$.map(note)
+    // music$: state$.map(note)
   };
+}
+
+function renderScoreGrid(score) {
+  return (
+    h(".score-grid", score.map(renderScoreRow))
+  )
+}
+
+function renderScoreRow({note, beats}) {
+  return (
+    h(".score-row", [
+      beats.map(renderBeatCell),
+      h('span.note-label', note)
+    ])
+  )
+}
+
+function renderBeatCell(cellEnabled) {
+  return  (
+    h("span.beat-cell", {style: {background: cellEnabled === 1 ? "pink" : ""}})
+  )
+}
+
+function renderBeatMarkerRow(beatOffset) {
+  return (
+    h(".beat-marker-row", [
+      h(".beat-marker", {style: {left: 42 * beatOffset + "px"}}, "V")
+    ])
+  )
 }
 
 run(main, {
